4874d9b43e7f71eb220a5c2201f101aab4a3b1cf
Adrian Epifanio <alu0101158280@ull.edu.es>
2020-03-13 17:48:12 +0000

    Arreglar caso base DyV

--- Practica 5 Producto de polinomios divide y vencerás/src/polinomio.cpp
+++ Practica 5 Producto de polinomios divide y vencerás/src/polinomio.cpp
@@ -17,7 +17,7 @@
 * @Author: Adrián Epifanio
 * @Date:   2020-03-11 17:59:48
 * @Last Modified by:   Adrián Epifanio
-* @Last Modified time: 2020-03-13 17:44:29
+* @Last Modified time: 2020-03-13 17:47:54
 */
 /*------------  FUNCTIONS DECLARATION  ------------*/
 
