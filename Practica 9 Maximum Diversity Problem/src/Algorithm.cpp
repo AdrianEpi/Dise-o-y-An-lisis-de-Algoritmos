9877c5249dfa841742bd1b97533728bf13041628
Adrian Epifanio <alu0101158280@ull.edu.es>
2020-05-06 18:10:30 +0100

    Upload Wed May  6 18:10:30 WEST 2020

--- Practica 9 Maximum Diversity Problem/src/Algorithm.cpp
+++ Practica 9 Maximum Diversity Problem/src/Algorithm.cpp
@@ -17,7 +17,7 @@
 * @Author: Adrián Epifanio
 * @Date:   2020-04-16 16:48:59
 * @Last Modified by:   Adrian Epifanio
-* @Last Modified time: 2020-05-06 17:26:10
+* @Last Modified time: 2020-05-06 18:06:30
 */
 /*----------  DECLARACION DE FUNCIONES  ----------*/
 
@@ -82,3 +82,44 @@ void Algorithm::set_MaxMean (float maxMean) {
 void Algorithm::runAlgorithm (Graph& graph, Chrono& chrono) {
 }
 
+/**
+ * @brief      Finds the diversity of a given vector of vertex.
+ *
+ * @param      vertex  The vertex
+ *
+ * @return     The diversity
+ */
+float Algorithm::findDiversity (std::vector<Vertex>& vertex) {
+    float diversity = 0.0;
+    for (int i = 0; i < (vertex.size() - 1); i++) {
+        for (int j = i + 1; j < vertex.size(); j++) {
+            float tmp;
+            for (int k = 0; k < vertex[0].get_Coordinates().size(); k++) {
+                tmp += pow((vertex[i].get_Coordinates()[k] - vertex[j].get_Coordinates[k]), 2);
+            }
+            diversity += tmp;
+        }
+    }
+    diversity = sqrt(diversity);
+    return diversity;
+}
+
+/**
+ * @brief      Calculates and generates the gravity center of a given vertex vector.
+ *
+ * @param      vertex  The vertex
+ *
+ * @return     The vector with the gravity center
+ */
+std::vector<float> Algorithm::generateGravityCenter (std::vector<Vertex>& vertex) {
+    std::vector<float> gravityCenter;
+    gravityCenter.resize(vertex.get_Coordinates().size());
+    for (int i = 0; i < vertex.get_Coordinates().size(); i++) {
+        gravityCenter[i] = 0;
+        for (int j = 0; j < vertex.size(); j++) {
+            gravityCenter[i] += vertex[j].get_Coordinates()[i];
+        }
+        gravityCenter[i] /= vertex.size();
+    }
+    return gravityCenter;
+}
